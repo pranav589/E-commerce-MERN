a2c0544ae8dead959253781d557ac81b9629b125
Pranav Bakale <pranav.bakale@gmail.com>
2021-06-28 17:25:36 +0530

    Done Backend Auth

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,12 +7,23 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 
+import { cartReducer } from "./reducers/cartReducers";
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const carrtItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: {
+    cartItems: carrtItemsFromStorage,
+  },
+};
 
 const middleware = [thunk];
 
